c1f9d37dfddf21551e34858e45de5e312f98dbb3
crazz-zaac <r.alpha919@gmail.com>
2024-01-05 18:46:43 +0100

    Update: Data picker and symbol selector for data preparation added

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import datetime
 from symbol_extract import ExtractSymbol
 from data_preparation import DataPreparation
+from streamlit_date_picker import date_range_picker, PickerType, Unit, date_picker
 
 st.title("Visualize and Explore Cryptocurrencies Data")
 
@@ -17,35 +18,46 @@ with st.sidebar.form(key='my_form'):
     # A selectbox for symbol selection in the sidebar inside the form
     crypto_symb = st.selectbox(
         'Select a crypto symbol',
-        symbols_list[:100]
+        symbols_list[:99]
     )
 
-    # start and end date for data
-    today = datetime.datetime.now().date()
-    start_year = datetime.date(2000, 1, 1)
-    end_year = today
+    # Use date_picker to create a date picker
+    st.subheader('Start Date Picker')
+    start_date_string = date_picker(picker_type=PickerType.time.string_value, value=0, unit=Unit.days.string_value,
+                            key='start_date_picker')
+
+    if start_date_string is None:
+        today = datetime.datetime.now()
+        start_date_string = (today - datetime.timedelta(days=1)).strftime('%Y-%m-%d %H:%M:%S')
     
-    date_range = st.date_input(
-        "Select date range for data",
-        (start_year, end_year),
-        start_year,
-        today,
-        format="YYYY-MM-DD",
-    )
+    st.subheader('End Date Picker')
+    end_date_string = date_picker(picker_type=PickerType.time.string_value, value=0, unit=Unit.days.string_value,
+                            key='end_date_picker')
+
+    if end_date_string is None:
+        today = datetime.datetime.now()
+        end_date_string = today.strftime('%Y-%m-%d %H:%M:%S')
     
-    print(date_range)
-    # start_date = int(datetime.datetime(start_year.year, start_year.month, start_year.day).timestamp())
-    # end_date = int(datetime.datetime(end_year.year, end_year.month, end_year.day).timestamp())
-    start_date = int(start_year.replace(hour=0, minute=0, second=0).timestamp()) * 1000
-    end_date = int(end_year.replace(hour=0, minute=0, second=0).timestamp()) * 1000
+    print(start_date_string, end_date_string)
+    
+    date_obj1 = datetime.datetime.strptime(start_date_string, '%Y-%m-%d %H:%M:%S')
+    date_obj2 = datetime.datetime.strptime(end_date_string, '%Y-%m-%d %H:%M:%S')
+    
+    start_date = datetime.datetime(date_obj1.year, date_obj1.month, date_obj1.day, date_obj1.hour, date_obj1.minute, date_obj1.second)
+    end_date = datetime.datetime(date_obj2.year, date_obj2.month, date_obj2.day, date_obj2.hour, date_obj2.minute, date_obj2.second)
+    
+    start_time = int(start_date.timestamp()) * 1000  # Multiply by 1000 to convert to milliseconds
+    end_time = int(end_date.timestamp()) * 1000  # Multiply by 1000 to convert to milliseconds
+    
+    print(start_time, end_time)
     
     # Add a form submit button
-    submit_button = st.form_submit_button(label='Submit')
+    submit_button = st.form_submit_button(label='Prepare Data')
     
-    # preparation of raw data
+    # Preparation of raw data
     if submit_button:
         if crypto_symb is not None:
-            data_prep = DataPreparation(crypto_symb, start_date, end_date)
+            data_prep = DataPreparation(crypto_symb, start_time, end_time)
             data_prep.getData()
         else:
             st.warning("Please select a crypto symbol before submitting")
