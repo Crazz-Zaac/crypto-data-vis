62a7b19bafa51333be2f5cbd3731f659e219617e
crazz-zaac <r.alpha919@gmail.com>
2024-01-09 22:04:54 +0100

    Updated: getStockData, visualizeData and CurrencyConvesion feature

--- src/compare_stocks.py
+++ src/compare_stocks.py
@@ -10,11 +10,16 @@ from streamlit_pagination import pagination_component
 
 class StockCompare:
     
-    def __init__(self, df):
-        self.df = df 
+    def __init__(self, amount, from_currency, to_currency):
+        self.file_path = DATASET_DIR + '/' + 'processed_data/crypto_data.csv'' 
+        self.amount = amount
+        self.frm_curr = from_currency
+        self.to_curr = to_currency
 
     def visualizeData(self):
-        
+        df = pd.read_csv(self.file_path+'crypto_data.csv')
+        # print(df)
+        # df.drop(columns=["Name", "52 Week Range", "Day Chart"], inplace=True, axis=1)
         style = """
         <style>
         th {
@@ -28,7 +33,7 @@ class StockCompare:
         """
         st.markdown(style, unsafe_allow_html=True)
         
-        # st.dataframe(self.df.style.highlight_max(axis=0), width=400, height=700)
+        # st.dataframe(df.style.highlight_max(axis=0), width=400, height=700)
         # Items per page
         items_per_page = 10
 
@@ -36,7 +41,7 @@ class StockCompare:
         page_number = st.number_input("Select a page number", min_value=1, value=1)
         
         # Calculate total number of pages
-        total_pages = math.ceil(len(self.df) / items_per_page)
+        total_pages = math.ceil(len(df) / items_per_page)
 
         # Calculate start and end indices for pagination
         start_index = (page_number - 1) * items_per_page
@@ -44,7 +49,7 @@ class StockCompare:
 
        
         # Display dataframe with pagination
-        styled_df = self.df.iloc[start_index:end_index].style.highlight_max(axis=0).set_table_styles(
+        styled_df = df.iloc[start_index:end_index].style.highlight_max(axis=0).set_table_styles(
             [{'selector': 'table', 
               'props': [('width', '800px'), ('height', '800px')]
               }]
@@ -57,32 +62,57 @@ class StockCompare:
         
         # statistical summaries
         
-        # st.dataframe(self.df.describe())
+        # st.dataframe(df.describe())
         
         #top performers
         
     
     def currencyConversion(self):
-        pass
+        df = pd.read_csv(self.file_path)
+        if from_currency not in df['Symbol'].values or to_currency not in df['Symbol'].values:
+            return None  # Invalid currency
+        
+        from_value = df.loc[df['Symbol'] == from_currency, 'Price'].values[0]
+        to_value = df.loc[df['Symbol'] == to_currency, 'Price'].values[0]
+
+        converted_amount = (amount * to_value) / from_value
+        return converted_amount
+        
+    
 
 
-@st.cache_data
-def getStockData():
-    file_path = DATASET_DIR + '/' + 'processed_data/' 
+def getStockData(file_path):
     session = HTMLSession()
     num_currencies = 250
     resp = session.get(f"https://finance.yahoo.com/crypto?offset=0&count={num_currencies}")
     tables = pd.read_html(resp.html.raw_html)
     crypto = tables[0].copy()
+    
+    # Remove unwanted columns
+    columns_to_drop = ["Name", "52 Week Range", "Day Chart", "Supply"]
+    crypto.drop(columns=columns_to_drop, inplace=True)
+    
     # Save to a CSV file
     crypto.to_csv(file_path+'crypto_data.csv', index=False)  # Saves the DataFrame to a CSV file
+    
+    # Get unique cryptocurrency symbols from the dataset
+    df = pd.read_csv(stkcmp.file_path + 'crypto_data.csv')
+    currencies = df['Symbol'].unique()
+    return currencies
 
 
 def main():
-    getStockData()
-    df = pd.read_csv(DATASET_DIR + '/' + 'processed_data/'+'crypto_data.csv')
-    df.drop(columns=["Name", "52 Week Range", "Day Chart"], inplace=True, axis=1)
-    stkcmp = StockCompare(df)
+    file_path = DATASET_DIR + '/' + 'processed_data/'
+    currency_list = getStockData(file_path)
+    
+    amount = st.number_input("Enter amount")
+    from_currency = st.selectbox("From currency", currencies)
+    
+    
+    
+    
+    stkcmp = StockCompare()
+    stkcmp.getStockData()
     stkcmp.visualizeData()
 
 
